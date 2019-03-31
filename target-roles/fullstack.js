c864fee444b127fc3dfcbc9bf25df004560c7cdd
Yonni Mendes <yonni@zazma.com>
2019-03-31 22:17:31 +0300

    Added expectations

--- target-roles/fullstack.js
+++ target-roles/fullstack.js
@@ -1,18 +1,19 @@
 export default [
   {
     displayName: 'Junior Full Stack Developer',
-    milestones: [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    milestones: [3,2,3,2,2,2,2,2,2,2],
   },
   {
     displayName: 'Intermediate Full Stack Developer',
-    milestones: [2, 1, 2, 2, 1, 2, 2, 1, 0, 0, 1, 1, 0, 0, 0, 1],
+    milestones: [3,3,3,3,3,3,2,3,3,2],
   },
   {
     displayName: 'Senior Full Stack Developer',
-    milestones: [3, 3, 3, 3, 2, 3, 3, 2, 1, 1, 2, 2, 1, 1, 1, 2],
+    milestones: [4,3,3,4,4,3,3,3,3,3],
+
   },
   {
     displayName: 'Full Stack Tech Lead',
-    milestones: [3, 3, 4, 4, 3, 3, 3, 3, 2, 1, 2, 2, 2, 2, 2, 2],
+    milestones: [4,4,4,4,4,4,3,3,3,3],
   },
 ];
