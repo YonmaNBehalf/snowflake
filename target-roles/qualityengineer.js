940e80f2d420a24a6b7096fb227750f339d989c1
Yonni Mendes <yonni@zazma.com>
2019-04-01 15:00:44 +0300

    Corrected QE expectations

--- target-roles/qualityengineer.js
+++ target-roles/qualityengineer.js
@@ -1,10 +1,10 @@
 export default [
   {
     displayName: 'Junior Quality Engineer',
-    milestones: [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    milestones: [3,2,3,2,2,2,0,0,2,2],
   },
   {
     displayName: 'Intermediate Quality Engineer',
-    milestones: [2, 1, 2, 2, 1, 2, 2, 1, 0, 0, 1, 1, 0, 0, 0, 1],
+    milestones: [3,3,3,3,3,3,0,0,2,2],
   }
 ];
